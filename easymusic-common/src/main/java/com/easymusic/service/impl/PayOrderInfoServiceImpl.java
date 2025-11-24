a3fe8ae89b1dc3b6c26f24b60c6834f4decf7098
cyt <1793402316@qq.com>
2025-11-24 13:54:15 +0800

    微信支付：验签

--- easymusic-common/src/main/java/com/easymusic/service/impl/PayOrderInfoServiceImpl.java
+++ easymusic-common/src/main/java/com/easymusic/service/impl/PayOrderInfoServiceImpl.java
@@ -17,8 +17,19 @@ import com.easymusic.service.PayOrderInfoService;
 import com.easymusic.utils.DateUtil;
 import com.easymusic.utils.StringTools;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.HttpUrl;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 
@@ -27,6 +38,7 @@ import java.util.List;
  * 支付订单信息 业务接口实现
  */
 @Service("payOrderInfoService")
+@Slf4j
 public class PayOrderInfoServiceImpl implements PayOrderInfoService {
 
     @Resource
@@ -189,4 +201,82 @@ public class PayOrderInfoServiceImpl implements PayOrderInfoService {
                 DateTimePatternEnum.YYYYMMDDHHMMSS.getPattern() +
                         StringTools.getRandomNumber(Constants.LENGTH_14).toLowerCase());
     }
+
+    public static void main(String[] args) {
+        String merchantId = "1900007291";
+        String cerSerialNo = "408B07E79B8269FEC3D5D3E6AB8ED163A6A380DB";
+        String sign = getToken4WeChat("POST", "https://api.mch.weixin.qq.com/v3/pay/transactions/native", "", merchantId,
+                cerSerialNo, "/Users/cyt/projects/i/cache/easymusic/key/apiclient_test_key.pem");
+        log.info("\n" + sign);
+    }
+
+    private static String getToken4WeChat(String method, String url, String body, String merchantId, String certSerialNo, String keyPath) {
+        HttpUrl httpUrl = HttpUrl.parse(url);
+        String nonceStr = StringTools.getRandomNumber(Constants.LENGTH_30);
+        long timestamp = System.currentTimeMillis() / 1000;
+        if (StringTools.isEmpty(body)) {
+            body = "";
+        }
+        String message = buildMessage(method, httpUrl, timestamp, nonceStr, body);
+        String signature = sign(message, keyPath);
+        String signstr = "mchid=\"" + merchantId + "\"," +
+                "nonce_str=\"" + nonceStr + "\"," +
+                "signature=\"" + signature + "\"," +
+                "timestamp=\"" + timestamp + "\"," +
+                "serial_no=\"" + certSerialNo + "\"";
+        return signstr;
+    }
+
+    private static String buildMessage(String method, HttpUrl url, long timestamp, String nonceStr, String body) {
+        String canonicalUrl = url.encodedPath();
+        if (url.encodedQuery() != null) {
+            canonicalUrl += "?" + url.encodedQuery();
+        }
+        return method + "\n" +
+                canonicalUrl + "\n" +
+                timestamp + "\n" +
+                nonceStr + "\n" +
+                body + "\n";
+    }
+
+    private static String sign(String str, String keyPath) {
+        try {
+            File file = new File(keyPath);
+            if (!file.exists()) {
+                throw new BusinessException("私钥文件不存在");
+            }
+            byte[] message = str.getBytes(StandardCharsets.UTF_8);
+            // 签名
+            Signature sign = Signature.getInstance("SHA256withRSA");
+            // 加载私钥
+            sign.initSign(getPrivateKey(keyPath));
+            // 开始签名
+            sign.update(message);
+            return new String(Base64.getEncoder().encode(sign.sign()));
+        } catch (BusinessException e) {
+            throw e;
+        } catch (Exception e) {
+            log.error("签名失败", e);
+            throw new BusinessException("生成签名失败");
+        }
+    }
+
+    private static PrivateKey getPrivateKey(String fileName) {
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
+            // 去除头尾
+            String privateKey = content
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "")
+                    .replaceAll("\\s+", "");
+            // 解析私钥
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            // 得到私钥对象
+            return kf.generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey)));
+        } catch (Exception e) {
+            log.error("读取私钥失败", e);
+            throw new BusinessException("读取私钥失败");
+        }
+    }
+
 }
